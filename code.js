2b7fcb790414b541895c0ad0c3357c2a39953d4f
Nicole Sweeden <sweeden.nicole@gmail.com>
2022-10-13 13:52:22 -0600

    add the first test and solution for intro-to-testing

--- code.js
+++ code.js
@@ -2,3 +2,19 @@
 function helloWorld() {
     return "Hello, World!";
 }
+
+function sayHello () {
+   return "Hello!";
+}
+//NOTES: idea behind test driven development is that we are incrementally adding to the function. Our test should be v exhaustive that at end, the only way to reasonably solve the problem of the function is by having function do what it is intended to do. TODAY, we are practicing how to write tests. NOT DOING: writing functions for tests. David tip: In assessment, just solve problem.
+
+/*
+function subtract(x, y) {
+    if (typeof x === "number" && typeof y === "number") {
+        return x - y;
+    } else {
+        return false;
+    }
+}
+
+ */
--- tests.js
+++ tests.js
@@ -1,15 +1,53 @@
 // Unit tests for the helloWorld function
-describe('helloWorld', function() {
-    it('should be a defined function', function() {
-      expect(typeof helloWorld).toBe('function');
+describe('helloWorld', function () {
+    it('should be a defined function', function () {
+        expect(typeof helloWorld).toBe('function');
     });
-    it('should return a string when called', function() {
-      expect(typeof helloWorld()).toBe("string");
+    it('should return a string when called', function () {
+        expect(typeof helloWorld()).toBe("string");
     });
-    it('should return the string "Hello, World!" when executed', function() {
-      expect(helloWorld()).toBe("Hello, World!");
+    it('should return the string "Hello, World!" when executed', function () {
+        expect(helloWorld()).toBe("Hello, World!");
     });
-    it("should never return 'undefined' when called", function() {
-      expect(helloWorld()).not.toBe(undefined);
+    it("should never return 'undefined' when called", function () {
+        expect(helloWorld()).not.toBe(undefined);
+    });
+});
+
+/*
+describe("subtract", function () {
+    it("should be a defined function", function () {
+        expect(typeof subtract).toBe("function");
+    });
+    it("should return a number when passed two number arguments", function () {
+        expect(typeof subtract(3, 1)).toBe("number");
+    });
+    it("it should return false when passed any argument that is not a number", function () {
+        expect(subtract()).toBe(false);
+        expect(subtract(2)).toBe(false);
+        expect(subtract("Banana", 6)).toBe(false);
+    })
+    it("should return 34 when passed the arguments of 64 and 30", function () {
+        expect(subtract(64, 30)).toBe(34);
+    });
+    it("should return 75 when passed the arguments of 100 and 25", function () {
+        expect(subtract(100, 25)).toBe(75);
+    });
+    it("should return -7 when passed the arguments of 0 and 7", function () {
+        expect(subtract(0, 7)).toBe(-7);
+    });
+    it("should return 295 when passed the arguments of 20 and -275", function () {
+        expect(subtract(20, -275)).toBe(295);
+    });
+    it("should return 0 when passed the arguments of 0 and 0", function () {
+        expect(subtract(0, 0)).toBe(0);
+    });
+});
+
+*/
+
+describe('sayHello', function () {
+    it('should be a defined function', function () {
+        expect(typeof sayHello).toBe('function');
     });
 });
